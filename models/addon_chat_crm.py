95232da83c2157f42306b5c977b3c4d5253c3505
Hoàng Bùi <buituhoang@gmail.com>
2021-04-28 08:13:43 +0700

    Check channel ID if in channel list or not.

--- models/addon_chat_crm.py
+++ models/addon_chat_crm.py
@@ -7,16 +7,25 @@ class AddonChatCRM(models.Model):
     _inherit = "crm.lead"
 
     def open_chat(self):
-        # channel = self.env
+        if self.channel_id:
+            channel = self.env['mail.channel'].sudo().search([('id', '=', self.channel_id.id)])
+            if channel:
+                channel.channel_fold(channel.uuid)
+            else:
+                new_channel = self.env['mail.channel'].sudo().create({
+                    'name': self.name,
+                })
+                self.write({'channel_id': new_channel.id})
+                self.channel_id.channel_fold(self.channel_id.uuid)
         if not self.channel_id:
             self.channel_id = self.env['mail.channel'].sudo().create({
                 'name': self.name,
                 # # 'channel_last_seen_partner_ids': [(0, 0, {'partner_id': self.partner_id})],
                 # 'channel_partner_ids': [(0, 0, {'partner_id': self.partner_id})]
             })
+            self.channel_id.channel_fold(self.channel_id.uuid)
         print(self.channel_id)
         print(self.channel_id.channel_partner_ids.name)
-        self.env['mail.channel'].channel_fold(self.channel_id.uuid)
 
     facebook_acc = fields.Char(string='Facebook')
     channel_id = fields.Many2one('mail.channel', 'Conversation', readonly=True)
